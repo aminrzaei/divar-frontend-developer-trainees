ac66e469165c930b92501a7c790c4b0ee0b783cb
Amin <my.aminrezaei@gmail.com>
2020-12-09 15:54:23 +0330

    widget created based on given ui and json

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,22 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+
+// eslint-disable-next-line no-unused-vars
+const widget = {
+  widget_type: 'FEATURE_ROW',
+  data: {
+    title: 'بررسی فنی بیش از <b>۴۰۰</b> نقطه‌ خودرو',
+    image_url: 'https://s100.divarcdn.com/static/icons/4/ic_checked.png',
+    image_color: 'SUCCESS_PRIMARY',
+    has_divider: true,
+    icon: {
+      image_url_dark:
+        'https://s100.divarcdn.com/static/imgs/widget-icons/dark/success_primary/checked.png',
+      image_url_light:
+        'https://s100.divarcdn.com/static/imgs/widget-icons/light/success_primary/checked.png',
+      icon_name: 'CHECKED',
+      icon_color: 'SUCCESS_PRIMARY',
+    },
+  },
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-intro">
+        <div className="widget-row">
+          <img
+            src={widget.data.icon.image_url_light}
+            className="widget-row__icon"
+            alt={widget.data.icon.icon_name}
+          />
+          <div
+            className="widget-row__title"
+            dangerouslySetInnerHTML={{ __html: `${widget.data.title}` }}
+          ></div>
+        </div>
       </header>
     </div>
   );
